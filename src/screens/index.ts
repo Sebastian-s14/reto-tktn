10fc20867d1c5f17e310629d80f5da6a375b2599
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2023-01-25 17:11:09 -0500

    feat: :sparkles: add screens

---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export { FavoritesScreen } from './FavoritesScreen'
+export { HomeScreen } from './HomeScreen'
+export { NavigationScreen } from './NavigationScreen'
+export { SettingsScreen } from './SettingsScreen'
