1451923de50f729306e6bd7e3c09e7b3a15b7a3a
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2023-01-26 01:30:51 -0500

    feat: :label: add Hotel type

---
+++ src/types/Hotel.ts
@@ -0,0 +1,11 @@
+export type Hotel = {
+  id: string
+  name: string
+  image: string
+  price: number
+  priceDescription: string
+  rate: number
+  stars: number
+  distance: string
+  description: string[]
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
-export * from './Navigation'
 export * from './Category'
+export * from './Hotel'
+export * from './Navigation'
