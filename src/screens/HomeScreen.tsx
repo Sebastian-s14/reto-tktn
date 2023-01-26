350558659d14043fb636cc4a3747820e8994aeb4
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2023-01-26 00:29:47 -0500

    feat: :sparkles: update HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { CategoryList, CustomIcon } from '@components'
+import { Spacer } from '@ui'
 
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text style={styles.headerTitle}>New York, US</Text>
+      <View style={styles.mainTitleContainer}>
+        <Text style={styles.mainTitle}>The best price</Text>
+        <CustomIcon icon="tune" color="#000" />
+      </View>
+      <Spacer />
+      <CategoryList />
+      <Spacer />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mainTitleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingVertical: 20,
+  },
+  mainTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+})
