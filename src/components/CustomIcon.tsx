3c9660f7bc9c666779f8a748d88429b645f533a8
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2023-01-26 22:05:08 -0500

    fix: :lipstick: update default color

--- src/components/CustomIcon.tsx
+++ src/components/CustomIcon.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { theme } from '@theme'
+
 interface CustomIconProps {
   icon: string
   color?: string
   size?: number
 }
 
-export const CustomIcon = ({ icon, color, size = 40 }: CustomIconProps) => (
-  <Icon name={icon} color={color} size={size} />
-)
+export const CustomIcon = ({
+  icon,
+  color = theme.secondaryColor,
+  size = 40,
+}: CustomIconProps) => <Icon name={icon} color={color} size={size} />
